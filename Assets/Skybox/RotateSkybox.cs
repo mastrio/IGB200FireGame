4657bff08d98d51057c14151d9e8cab7bec4b757
Andrew <n12024724@qut.edu.au>
2025-10-01 19:37:07 +1000

    Skybox fix

--- Assets/Skybox/RotateSkybox.cs
+++ Assets/Skybox/RotateSkybox.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 public class RotateSkybox : MonoBehaviour
 {
     public float RotateSpeed = 1.0f;
+    private Material skyboxInstance;
 
+    void Start()
+    {
+        skyboxInstance = new Material(RenderSettings.skybox); // Clone skybox material so edits are not made to the original asset.
+        RenderSettings.skybox = skyboxInstance;
+    }
 
     void Update()
     {
-        RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotateSpeed);
+        skyboxInstance.SetFloat("_Rotation", Time.time * RotateSpeed); // Rotate the cloned instance only.
     }
 }
