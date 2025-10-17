a2a9e9a182608990c4f1548f0c3c3efd120bbdec
mastrio <mastrio5772@gmail.com>
2025-10-17 18:07:07 +1000

    fixed the wind ui
    that was stupid

--- Assets/Scripts/UI/WindParticleUI.cs
+++ Assets/Scripts/UI/WindParticleUI.cs
@@ -9,7 +9,7 @@ public class WindParticleUI : MonoBehaviour
         particlesObject.transform.localRotation = Quaternion.Euler(new Vector3(
             0.0f,
             0.0f,
-            WindManager.instance.directionDegrees - 90.0f
+            -WindManager.instance.directionDegrees
         ));
     }
 }
