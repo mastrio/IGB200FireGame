b0984ca5632352c315703c51cad57dea06e0322d
Huu Duc Huy Bui <n11680679@qut.edu.au>
2025-10-18 21:35:59 +1000

    fixture of volume slider bug
    i made a volume slider script and volume settings manager script to make the volume slider keep changes from moving scene to scene until quit the game

---
+++ Assets/Scripts/Core/VolumeSlider.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    private void Start()
+    {
+        if (VolumeSettingsManager.Instance == null) return;
+
+        if (musicSlider != null)
+        {
+            musicSlider.value = VolumeSettingsManager.Instance.GetMusicVolume();
+            musicSlider.onValueChanged.AddListener(VolumeSettingsManager.Instance.SetMusicVolume);
+        }
+        if (sfxSlider != null)
+        {
+            sfxSlider.value = VolumeSettingsManager.Instance.GetSFXVolume();
+            sfxSlider.onValueChanged.AddListener(VolumeSettingsManager.Instance.SetSFXVolume);
+        }
+    }
+
+    public void SyncSliders()
+    {
+        if (musicSlider != null) musicSlider.value = VolumeSettingsManager.Instance.GetMusicVolume();
+        if (sfxSlider != null) sfxSlider.value = VolumeSettingsManager.Instance.GetSFXVolume();
+    }
+}
