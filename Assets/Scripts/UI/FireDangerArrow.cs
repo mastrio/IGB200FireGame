24436530746af67676fd1fa831596cfb6472ca8b
Nero393 <167185905+Nero393@users.noreply.github.com>
2025-10-21 20:28:52 +1000

    Fire danger update part 2

--- Assets/Scripts/UI/FireDangerArrow.cs
+++ Assets/Scripts/UI/FireDangerArrow.cs
@@ -9,7 +9,7 @@ public class FireDangerArrow : MonoBehaviour
 
     void Update()
     {
-        int clampValue = Mathf.Clamp(FireManager.instance.FireDangerLevel, 0, 60);
+        int clampValue = Mathf.Clamp(FireManager.instance.FireDangerLevel, 0, 90);
         //If you want to change it basically need the max to be something that will be able to = 180 max for the the clamp value so 30 * 6, 60 * 3, 90 *2 
         float angle = 90 - (clampValue*3);
 
