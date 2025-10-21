b7ca70526738afc649fecea9202ee3d3aa1e8ddc
Nero393 <167185905+Nero393@users.noreply.github.com>
2025-10-21 20:35:33 +1000

    Fixed

--- Assets/Scripts/UI/FireDangerArrow.cs
+++ Assets/Scripts/UI/FireDangerArrow.cs
@@ -11,7 +11,7 @@ public class FireDangerArrow : MonoBehaviour
     {
         int clampValue = Mathf.Clamp(FireManager.instance.FireDangerLevel, 0, 90);
         //If you want to change it basically need the max to be something that will be able to = 180 max for the the clamp value so 30 * 6, 60 * 3, 90 *2 
-        float angle = 90 - (clampValue*3);
+        float angle = 90 - (clampValue*2);
 
         needle.localRotation = Quaternion.Euler(0f, 0f, angle);
 
